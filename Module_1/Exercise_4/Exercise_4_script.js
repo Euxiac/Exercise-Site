0fdff53c51bd91d0d1a8171313e427dcb7ce3cf6
Christie <christie.tanc@gmail.com>
2024-07-22 23:19:56 +0800

    Exercise 4 update

--- Module_1/Exercise_4/Exercise_4_script.js
+++ Module_1/Exercise_4/Exercise_4_script.js
@@ -1,9 +1,46 @@
+/*
+Lab Exercise 4
+
+Use the learnings from this lesson to design some very simple
+functions. Call them with different values.
+
+1. Create 4 functions for the 4 main mathematical operations
+(-,+,/,*). Return the calculated value and then output it to the
+screen.
+
+2. Create a function that takes the name of a person as an
+argument, and prints out “Hello <name>” to the console.
+Hint: search online on how to concatenate two strings.
+*/
+
 const a = 4;
 const b = 2;
 const name = "Steven";
 
-function multiply(a,b){
-return a*b;
+function subtract(a, b) {
+  return a - b;
 }
 
-console.log("My name is " + name + " and " + a + " x " + b + " = " + multiply(6,1));
+function add(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function hello(name) {
+    console.log("Hello " + name);
+  }
+
+console.log("test vars are a=" + a + " abd b=" + b + ". test name is " + name);
+console.log(a + "-" + b + "="+ subtract(a,b));
+console.log(a + "-" + b + "="+ add(a,b));
+console.log(a + "-" + b + "="+ divide(a,b));
+console.log(a + "-" + b + "="+ multiply(a,b));
+hello(name);
+
