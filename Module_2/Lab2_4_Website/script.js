bd1335d9db2d21d4412b77b5d3e8c9d5ab01299f
Christie <christie.tanc@gmail.com>
2024-08-07 16:14:17 +0800

    Module 2, Lab2.4 Website

---
+++ Module_2/Lab2_4_Website/script.js
@@ -0,0 +1,51 @@
+const jobHistory = [
+  {
+    title: "Web developer",
+    company: "AwesomeCo",
+    date: "December 2021-Current",
+    responsibilities: [
+      "Creating beautiful websites with use of HTML, and CSS",
+      "Following best accessibility principles",
+      "Solving problems and proposing smooth, working solutions",
+    ],
+  },
+  {
+    title: "Receptionist",
+    company: "BuildCo",
+    date: "January 2019 - June 2021",
+    responsibilities: [
+      "Creating beautiful websites with use of HTML, and CSS",
+      "Following best accessibility principles",
+      "Solving problems and proposing smooth, working solutions",
+    ],
+  },
+];
+
+const educationHistory = [
+    {
+        qualification: 'Institute of Data Web Development Course',
+        institution: 'Institute of Data',
+        date: 'September 2021 - 2021',
+        skills: [
+            'Web development',
+            'HTML',
+            'CSS',
+            'React',
+            'node.js',
+            'express servers'
+        ]
+    },
+    {
+        qualification: 'Bachelors',
+        institution: 'University',
+        date: 'September 2021 - 2021',
+        skills: [
+            'Web development',
+            'HTML',
+            'CSS',
+            'React',
+            'node.js',
+            'express servers'
+        ]
+    }
+]
