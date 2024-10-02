2933728617e8741aa1a09a7522ef1587979ce151
Christie <christie.tanc@gmail.com>
2024-10-02 19:02:20 +0800

    module 7 catch up

--- Module_7/Hospital/hospital_be/index.js
+++ Module_7/Hospital/hospital_be/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const bodyParser = require("body-parser");
-const sequelize = require("./config/database");
+import express from "express";
+import bodyParser from "body-parser";
+import sequelize from "./config/database.js";
+import patientRoutes from "./routes/patientRoutes.js";
 
 // Initialize the app
 const app = express();
@@ -10,10 +11,8 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(express.json()); // To parse JSON request body
 
-// Allow all origins or specify domain
-// this bit is about bypassing the CORS policy for local connections. see enable-cors.org
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins or specify domain
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -21,35 +20,17 @@ app.use((req, res, next) => {
   next();
 });
 
-//ideally line 14-26 should be seperated into controllers/routes and services
-// Define raw SQL queries
-const queries = {
-  getAllPatients: "SELECT * FROM Patients",
-};
-
-//endpoints
-app.get("/patients", async (req, res) => {
-  try {
-    const [results] = await sequelize.query(queries.getAllPatients);
-    res.json(results);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// Define the routes
+app.use("/api", patientRoutes);
 
 // Sync the database and start the server
-sequelize
-  .sync()
-  .then(() => {
-
-    app.get('/', (req, res) => {
-      res.send('Server Running');
-    });
-
+(async () => {
+  try {
+    await sequelize.sync();
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+  } catch (err) {
+    console.error("Unable to connect to the database:", err.message);
+  }
+})();
