84dbec9dde80fc810abac96f4eaeeb5f3fba2f0f
Christie <christie.tanc@gmail.com>
2024-10-07 21:50:26 +0800

    Module 7 ex1 and class exercise

---
+++ Module_7/Class Exercises/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+
+import { useState, useEffect } from "react";
+import UserList from './component/UserList';
+
+function App() {
+  return (
+    <div>
+      <h1>Class Exercises</h1>
+      <UserList/>
+    </div>
+  );
+}
+
+export default App;
