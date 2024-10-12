1130d80c33d96965aac7da6a6cab9d81b5047370
Christie <christie.tanc@gmail.com>
2024-10-12 09:50:18 +0800

    classwork

--- Module_7/Class Exercises/src/App.jsx
+++ Module_7/Class Exercises/src/App.jsx
@@ -2,12 +2,16 @@
 
 import { useState, useEffect } from "react";
 import UserList from './component/UserList';
+import CountdownTimer from './component/CountdownTimer';
+import Counter from './component/Counter';
 
 function App() {
   return (
     <div>
       <h1>Class Exercises</h1>
       <UserList/>
+      <CountdownTimer/>
+      <Counter/>
     </div>
   );
 }
